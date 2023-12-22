97a9c23b697e0260733aa93c1771d50bc95d763d
KoushikSarkarGit <sarkarkoushik557@gmail.com>
2023-12-22 23:31:11 +0530

    added tag button details

--- client/src/components/SharePostComponent.js
+++ client/src/components/SharePostComponent.js
@@ -13,6 +13,8 @@ export default function SharePostComponent() {
 
     const [postimage, setpostimage] = useState();
     const [taglist, setTaglist] = useState([{ "tagname": "tagdfdfdfdfdf1" }, { "tagname": "tag2" }, { "tagname": "tag3" }]);
+    const [tagmodal, setTagmodal] = useState(false);
+
 
     const postimgref = useRef()
 
@@ -70,29 +72,33 @@ export default function SharePostComponent() {
 
                     </div>}
 
-                    <div className='tagbox '>
-                        <div className='d-flex flex-wrap px-2 py-2 my-1'>
+                    {
+                        tagmodal && <div className='tagbox '>
+                            <div className='d-flex flex-wrap px-2 py-2 my-1'>
 
-                            <span className='me-2'> <b>Tags:</b></span>
+                                <span className='me-2'> <b>Tags:</b></span>
 
-                            {
-                                taglist.map((item, index) => {
-                                    return <div className="d-flex  align-items-center mx-1 py-1 badge text-bg-primary" key={index + 's'}>
-                                        #{item.tagname}
-                                        <RxCross1 className='ms-2 indivtagcross' type='button' />
-                                        {/* <button type="button" className="btn-close ms-2 " data-bs-dismiss="modal" aria-label="Close"></button> */}
-                                    </div>
-                                })
-                            }
+                                {
+                                    taglist.map((item, index) => {
+                                        return <div className="d-flex  align-items-center mx-1 py-1 badge text-bg-primary" key={index + 's'}>
+                                            #{item.tagname}
+                                            <RxCross1 className='ms-2 indivtagcross' type='button' />
+                                            {/* <button type="button" className="btn-close ms-2 " data-bs-dismiss="modal" aria-label="Close"></button> */}
+                                        </div>
+                                    })
+                                }
 
+                            </div>
+
+                            <div className="input-group mb-3">
+
+                                <input type="text" class="form-control py-2" placeholder="Enter tag here" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                                <button className="btn btn-outline-primary" type="button" id="button-addon2">Add tag</button>
+                            </div>
                         </div>
+                    }
 
-                        <div className="input-group mb-3">
 
-                            <input type="text" class="form-control py-2" placeholder="Enter tag here" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                            <button className="btn btn-outline-primary" type="button" id="button-addon2">Add tag</button>
-                        </div>
-                    </div>
 
 
 
@@ -115,9 +121,17 @@ export default function SharePostComponent() {
 
                             Schedule
                         </div>{" "}
-                        <div className="feature" style={{ color: "blue" }}>
+                        <div className={`feature + ${tagmodal ? 'clickedfeature' : ''}`} style={{ color: "blue" }}
+                            onClick={() => {
+                                if (tagmodal === true) {
+                                    setTagmodal(false)
+                                }
+                                else {
+                                    setTagmodal(true)
+                                }
+                            }}  >
 
-                            <UilLabelAlt className='sharefeaturecomp' />
+                            <UilLabelAlt className='sharefeaturecomp ' />
 
                             Add Tag
                         </div>
